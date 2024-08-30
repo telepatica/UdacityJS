1f2368add647bea20bac73e83ac7539db5ee5e90
Brittany Salas <brittanysalas90@gmail.com>
2024-08-30 14:57:48 -0700

    complete project

--- src/routes/resizeparams.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var sharp_1 = require("../utilities/sharp");
-var routes = express.Router();
-routes.get('/ImageProcessor', sharp_1["default"], function (req, res) {
-    res.sendFile('/Users/brittanysalas/Documents/ImageProessingAPI/assets/treelady.png');
-});
-exports["default"] = routes;
--- src/tests/indexSpec.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//import myFunc from '../index';
-//it('expect myFunc(5) to equal 25', () => {
-//expect(myFunc(5)).toEqual(25);
-//});
